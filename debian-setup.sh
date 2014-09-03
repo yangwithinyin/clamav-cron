54d5e3cae9c8525ad314701f05f81acd8f3dac79
yangwithinyin <yangwithinyin@gmail.com>
2014-09-03 14:34:31 -0400

    bump to 0.8.2, symlink clamdscan
    Enable Linux daemon functionality.

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # clamav-cron v. 0.6 - Copyright 2009, Stefano Stagnaro
-# clamav-cron easy v. 0.8.1 - Created by Mark Parraway
+# clamav-cron easy v. 0.8.2 - Created by Mark Parraway
 # This is Free Software released under the GNU GPL license version 3
 #
-# debian-setup.sh - quick debian/ubuntu setup script tested on Ubuntu 12.04 LTS
+# debian-setup.sh - quick Debian based setup script tested on Ubuntu 12.04 LTS
 #
 
 #============================================#
@@ -17,9 +17,10 @@ wget https://raw.githubusercontent.com/yangwithinyin/clamav-cron/master/clamav-c
 ln -s /usr/sbin/sendmail /bin/mail
 cp clamav-cron.sh /usr/local/bin/clamav-cron.sh
 chmod 755 /usr/local/bin/clamav-cron.sh
+ln -s /usr/bin/clamdscan /usr/local/bin/clamscan
 chown clamav:clamav /usr/local/bin/clamav-cron.sh
 chown clamav:clamav /var/log/clamav
 chown root: /var/lib/clamav
 chmod g+w /var/lib/clamav
 touch /var/log/clamav/clamav-cron.log
-
+/usr/bin/freshclam
