97133e46b778d3dbb294e6b5e5e660124e567eaa
yangwithinyin <yangwithinyin@gmail.com>
2014-09-03 14:51:19 -0400

    Complete with comments

--- freebsd-setup.sh
+++ freebsd-setup.sh
@@ -11,20 +11,33 @@
 #        DO NOT EDIT DO NOT EDIT             #
 #============================================#
 
+# Download clamav-cron.sh
+
 cd ~
 wget --no-check-certificate https://raw.githubusercontent.com/yangwithinyin/clamav-cron/master/clamav-cron.sh
 cp clamav-cron.sh /usr/local/bin/clamav-cron.sh
+
+# Make sure to configure/setup sendmail
+
+# Update ports
+
 portsnap update
 portsnap fetch
-# Setup sendmail in /etc/rc.conf (Default=ON)
+
+# Add clamav and update definitions
+
 pkg_add -r clamav
+ln -s /usr/local/bin/clamscan  /usr/bin/clamscan
+ln -s /usr/local/bin/freshclam /usr/bin/freshclam
+ln -s /usr/sbin/sendmail /bin/mail
+chown -R clamav:clamav /var/log/clamav
+touch /var/log/clamav/clamav-cron.log
+freshclam
+
+# FreeBSD specific daemon settings and startup
+
 echo '## Enable clamav ##' >> /etc/rc.conf
 echo 'clamav_clamd_enable="YES"' >> /etc/rc.conf
 echo 'clamav_freshclam_enable="YES"' >> /etc/rc.conf
 /usr/local/etc/rc.d/clamav-clamd start
 /usr/local/etc/rc.d/clamav-freshclam start
-ln -s /usr/local/bin/clamscand  /usr/bin/clamscan
-ln -s /usr/local/bin/freshclam /usr/bin/freshclam
-ln -s /usr/sbin/sendmail /bin/mail
-chown -R clamav:clamav /var/log/clamav
-touch /var/log/clamav/clamav-cron.log
