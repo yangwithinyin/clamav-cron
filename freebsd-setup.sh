ad7d2ddc98bd25193543b2a6257c02f558b4e7c2
yangwithinyin <yangwithinyin@gmail.com>
2014-09-03 14:24:49 -0400

    bump to 0.8.2
    FreeBSD includes clamav daemon functionality

--- freebsd-setup.sh
+++ freebsd-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # clamav-cron v. 0.6 - Copyright 2009, Stefano Stagnaro
-# clamav-cron easy v. 0.8.1 - Created by Mark Parraway
+# clamav-cron easy v. 0.8.2 - Created by Mark Parraway
 # This is Free Software released under the GNU GPL license version 3
 #
 # freebsd-setup.sh - quick FreeBSD setup script tested on FreeBSD 8.4-RELEASE
@@ -23,7 +23,7 @@ echo 'clamav_clamd_enable="YES"' >> /etc/rc.conf
 echo 'clamav_freshclam_enable="YES"' >> /etc/rc.conf
 /usr/local/etc/rc.d/clamav-clamd start
 /usr/local/etc/rc.d/clamav-freshclam start
-ln -s /usr/local/bin/clamscan  /usr/bin/clamscan
+ln -s /usr/local/bin/clamscand  /usr/bin/clamscan
 ln -s /usr/local/bin/freshclam /usr/bin/freshclam
 ln -s /usr/sbin/sendmail /bin/mail
 chown -R clamav:clamav /var/log/clamav
